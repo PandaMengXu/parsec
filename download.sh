99854c9dd7ee667d605dab0972d55db445282eb6
Meng Xu <mengxu@cis.upenn.edu>
2015-04-19 10:49:22 -0400

    Note: How to run parsec benchmarks
    	modified:   download.sh
    	new file:   note-experiment-cmd.txt
    	new file:   note.txt

--- download.sh
+++ download.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-wget http://parsec.cs.princeton.edu/download/3.0/parsec-3.0.tar.gz
+#wget http://parsec.cs.princeton.edu/download/3.0/parsec-3.0.tar.gz
+
+#Kim uses parsec-2.1 in his paper
+#This is the complete version that contains input
+#http://parsec.cs.princeton.edu/download/2.1/parsec-2.1.tar.gz
